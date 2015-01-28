41209d33ed946e1931b71b04826f97c5148dda5f
a5chan <allenzery@hotmail.com>
2015-01-28 17:09:31 -0500

    Starting up with database connections

---
+++ leagueapp/engine/database_functions.php
@@ -0,0 +1,18 @@
+<?php
+include_once ("database_basefunctions.php");
+
+
+
+function getusernamelist(){
+ 
+    $data = getsinglevaluearray('user_name', 'select user_name from users' );
+    return $data;
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
